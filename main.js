329407ded09d2155f0a28db6877ae508f9b8078c
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-20 17:02:05 +0200

    feat: generate post content whith js

--- main.js
+++ main.js
@@ -65,3 +65,43 @@ const posts = [
 const postContainer = document.getElementById("container");
 
 // passo un forEach sull'array per creare il mio html per ogni oggetto presente inserendo i valori degli oggetti al posto dell'html
+posts.forEach((element) => {
+  postContainer.innerHTML += `<div class="post">
+        <div class="post__header">
+          <div class="post-meta">
+            <div class="post-meta__icon">
+              <img
+                class="profile-pic"
+                src="${element.author.image}"
+                alt="Phil Mangione"
+              />
+            </div>
+            <div class="post-meta__data">
+              <div class="post-meta__author">${element.author.name}</div>
+              <div class="post-meta__time">${element.created}</div>
+            </div>
+          </div>
+        </div>
+        <div class="post__text">${element.content}
+        </div>
+        <div class="post__image">
+          <img src="${element.media}" alt="" /></div>
+        <div class="post__footer">
+          <div class="likes js-likes">
+            <div class="likes__cta">
+              <a class="like-button js-like-button" href="#" data-postid="1">
+                <i
+                  class="like-button__icon fas fa-thumbs-up"
+                  aria-hidden="true"
+                ></i>
+                <span class="like-button__label">Mi Piace</span>
+              </a>
+            </div>
+            <div class="likes__counter">
+              Piace a
+              <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+            </div>
+          </div>
+        </div>
+      </div>`;
+});
